bfc726603c3544f9227f2f2a275ee4fcb8490d6a
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-05-02 05:08:42 +0600

    json file added and data showing on site

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefDetails = require("./data/chefDetails.json")
+
 app.get("/", (req, res)=> {
     res.send("Site is running")
+})
+
+app.get("/chef_details", (req, res) => {
+    res.send(chefDetails)
 })
 
 app.listen(port, () => {
