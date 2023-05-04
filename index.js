15e8ea1865ab9769e4ab8b5183ea24772206b5c5
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-05-04 06:26:55 +0600

    image size reduse

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.get("/food_details", (req, res) => {
   res.send(foodDetails);
 });
 
+app.get("/chef_details/:id", (req, res) => {
+  const id = req.params.id;
+  const chefId = chefDetails.find((e) => e.id == id);
+  res.send(chefId);
+})
+
 app.listen(port, () => {
   console.log(`site api is running port: ${port}`);
 });
